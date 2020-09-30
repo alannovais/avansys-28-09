5094a58b90d9ec2583a0248351fcc6373866913a
Alan Novais <alannovais@hotmail.com>
2020-09-30 17:14:00 -0300

    ajuste

--- client-operadora/src/components/HelloWorld.vue
+++ client-operadora/src/components/HelloWorld.vue
@@ -1,129 +1,187 @@
 <template>
-  <div class="hello">
-    <h1>Manter Operadora</h1>
-    <div>
-      <button
-        type="button"
-        class="btn"
-        style="
-          border-radius: 20px;
-          background-color: #ccc7;
-          box-shadow: 0.5px 0.5px #0004;
-        "
-      >
-        <b-icon-plus /> Novo
-      </button>
-      <b-icon-pencil />
-      <b-icon-x />
-      <b-form-checkbox switch size="sm">Small</b-form-checkbox>
-    </div>
-    <div>
-      <b-form-checkbox
-        id="checkbox-1"
-        v-model="allRecords"
-        name="checkbox-1"
-        value="accepted"
-        unchecked-value="not_accepted"
-      />
-      <button
-        type="button"
-        class="btn"
-        style="
-          border-radius: 5px;
-          background-color: #ccc7;
-          box-shadow: 0.5px 0.5px #0004;
-        "
-      >
-        <b-icon-trash />&nbsp;Excluir
-      </button>
-      <button
-        type="button"
-        class="btn"
-        style="
-          border-radius: 5px;
-          background-color: #ccc7;
-          box-shadow: 0.5px 0.5px #0004;
-        "
-      >
-        <b-icon-sliders />&nbsp;Habilitar ou Desabilitar
-      </button>
-    </div>
-
-    <div>
+  <b-container>
+    <div class="hello">
+      <h1>Manter Operadora</h1>
+      <div>
+        <button
+          type="button"
+          class="btn"
+          style="
+            border-radius: 20px;
+            background-color: #ccc7;
+            box-shadow: 0.5px 0.5px #0004;
+          "
+          @click="manageData"
+        >
+          <b-icon-plus /> Novo
+        </button>
+      </div>
+      <div>
+        <b-form-checkbox
+          id="checkbox-1"
+          v-model="allRecords"
+          name="checkbox-1"
+          value="accepted"
+          unchecked-value="not_accepted"
+        />
+        <button
+          type="button"
+          class="btn"
+          style="
+            border-radius: 5px;
+            background-color: #ccc7;
+            box-shadow: 0.5px 0.5px #0004;
+          "
+        >
+          <b-icon-trash />&nbsp;Excluir
+        </button>
+        <button
+          type="button"
+          class="btn"
+          style="
+            border-radius: 5px;
+            background-color: #ccc7;
+            box-shadow: 0.5px 0.5px #0004;
+          "
+          @click="changeStatus"
+        >
+          <b-icon-sliders />&nbsp;Habilitar ou Desabilitar
+        </button>
+      </div>
       <form class="form-inline">
         <label for="">Operadora</label><br />
-        <b-form-select v-model="selected" :options="status"></b-form-select>
+        <b-form-select
+          v-model="selectedOperadora"
+          :options="operadoraDropDown"
+        ></b-form-select>
         <label for="">Status</label><br />
         <b-form-select v-model="selected" :options="status"></b-form-select>
       </form>
-    </div>
-    <b-table
-      striped
-      hover
-      ref="selectableTable"
-      selectable
-      :select-mode="selectMode"
-      @row-selected="onRowSelected"
-      :fields="fields"
-      :items="items"
-    />
-    <div>
-      <b-button id="show-btn" @click="showModal">Open Modal</b-button>
-      <b-button id="toggle-btn" @click="toggleModal">Toggle Modal</b-button>
-
-      <b-modal ref="my-modal" hide-footer title="Using Component Methods">
-        <div class="d-block text-center">
-          <h3>Hello From My Modal!</h3>
-        </div>
-        <b-button class="mt-3" variant="outline-danger" block @click="hideModal"
-          >Close Me</b-button
-        >
-        <b-button
-          class="mt-2"
-          variant="outline-warning"
-          block
-          @click="toggleModal"
-          >Toggle Me</b-button
-        >
-      </b-modal>
-    </div>
-    <div
-      class="modal fade"
-      id="staticBackdrop"
-      data-backdrop="static"
-      data-keyboard="false"
-      tabindex="-1"
-      aria-labelledby="staticBackdropLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
+      <b-table
+        striped
+        hover
+        ref="selectableTable"
+        selectable
+        :select-mode="selectMode"
+        @row-selected="onRowSelected"
+        :fields="fields"
+        :items="items"
+      >
+        <template v-slot:cell(status)="row">
+          <b-container>
+            <b-row>
+              <b-button variant="link" @click="manageData(row.item)">
+                <b-icon-pencil />
+              </b-button>
+              <b-form-checkbox
+                v-model="row.item.status"
+                switch
+                size="sm"
+                style="margin-top: 5.5px"
+              />
+              <b-button variant="link" @click="deleteModal(row.item)">
+                <b-icon-x />
+              </b-button>
+            </b-row>
+          </b-container>
+          <!-- <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? "Hide" : "Show" }} Details
+        </b-button> -->
+        </template>
+      </b-table>
+      <div>
+        <b-modal ref="my-modal" hide-footer title="Gerenciamento Operadora">
+          <div class="d-block text-center">
+            <form class="form-inline">
+              <b-form-input
+                v-model="operadora"
+                placeholder="Digite Operadora"
+                required
+              ></b-form-input>
+              <b-form-input
+                v-model="descricao"
+                placeholder="Digite Descrição"
+              ></b-form-input>
+            </form>
           </div>
-          <div class="modal-body">...</div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">Understood</button>
+          <b-button
+            class="mt-3"
+            variant="outline-danger"
+            block
+            @click="hideModal"
+            >Cancelar</b-button
+          >
+          <b-button
+            class="mt-2"
+            variant="outline-warning"
+            block
+            @click="saveData"
+            >Salvar</b-button
+          >
+        </b-modal>
+
+        <b-modal ref="modal-delete">
+          <div class="d-block text-center">
+            <h3>
+              Tem certeza que deseja excluir a operadora
+              <span v-if="itemDelete">{{ itemDelete.operadora }}</span
+              >?
+            </h3>
+          </div>
+          <b-button
+            class="mt-3"
+            variant="outline-danger"
+            block
+            @click="hideModal"
+            >Não</b-button
+          >
+          <b-button
+            class="mt-2"
+            variant="outline-warning"
+            block
+            @click="deleteData(itemDelete.id)"
+            >Sim</b-button
+          >
+        </b-modal>
+      </div>
+      <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-backdrop="static"
+        data-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">Understood</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </b-container>
 </template>
 
 <script>
@@ -141,6 +199,9 @@ import {
   BootstrapVueIcons,
   BFormCheckboxGroup,
   FormCheckboxPlugin,
+  BContainer,
+  LayoutPlugin,
+  FormInputPlugin,
 } from "bootstrap-vue";
 
 Vue.use(BootstrapVueIcons);
@@ -148,7 +209,10 @@ Vue.use(FormSelectPlugin);
 Vue.use(ButtonPlugin);
 Vue.use(ModalPlugin);
 Vue.use(FormCheckboxPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(FormInputPlugin);
 
+Vue.component("b-container", BContainer);
 Vue.component("b-form-checkbox-group", BFormCheckboxGroup);
 Vue.component("b-form-select", BFormSelect);
 Vue.component("b-button", BButton);
@@ -162,21 +226,14 @@ export default {
       selectMode: "multi",
       selected: null,
       allRecords: false,
-      status: [
+      descricao: "",
+      selectedOperadora: null,
+      operadoraDropDown: [
         {
           value: null,
           text: "Todos",
         },
-        {
-          value: 1,
-          text: "Ativo(s)",
-        },
-        {
-          value: 2,
-          text: "Inativo(s)",
-        },
       ],
-      allChecked: false,
       fields: [
         {
           key: "operadora",
@@ -187,24 +244,62 @@ export default {
           label: "Descrição",
         },
       ],
-      items: [
-        { edit: true, operadora: 40, descricao: "Dickerson" },
-        { edit: true, operadora: 21, descricao: "Larsen" },
-        { edit: true, operadora: 89, descricao: "Geneva" },
-        { edit: true, operadora: 38, descricao: "Jami" },
+      // items: [
+      //   { status: false, operadora: 40, descricao: "Dickerson" },
+      //   { status: true, operadora: 21, descricao: "Larsen" },
+      //   { status: true, operadora: 89, descricao: "Geneva" },
+      //   { status: true, operadora: 38, descricao: "Jami" },
+      // ],
+      itemDelete: "",
+      status: [
+        {
+          value: null,
+          text: "Todos",
+        },
+        {
+          value: 1,
+          text: "Ativo(s)",
+        },
+        {
+          value: 0,
+          text: "Inativo(s)",
+        },
       ],
+      operadora: "",
     };
   },
   methods: {
-    onRowSelected(data) {
-      let arr = [];
-      if (data.length) {
-          // arr.push({key: 'edit', label: 'Editar'});
-          arr.push(this.fields);
-      } else {
-        arr.slice(0);
+    manageData(e = null) {
+      this.operadora = "";
+      this.descricao = "";
+      if (e != null) {
+        this.operadora = e.operadora;
+        this.descricao = e.descricao;
       }
-      this.fields = arr;
+      this.$refs["my-modal"].toggle("#toggle-btn");
+    },
+
+    saveData() {
+      if (this.operadora && this.descricao) {
+        return this.postData({
+          operadora: this.operadora,
+          descricao: this.descricao,
+          status: 1,
+        });
+      }
+    },
+    changeStatus() {
+      this.items.map((x) => (x.status = !x.status));
+    },
+    onRowSelected(data) {
+      if (this.allRecords === "accepted" || data.length > 0) {
+        return this.fields.unshift({
+          key: "status",
+          label: "Editar",
+        });
+      } else if (data.length === 0) {
+        return this.fields.shift();
+      }
     },
 
     showModal() {
@@ -215,10 +310,11 @@ export default {
       this.$refs["my-modal"].hide();
     },
 
-    toggleModal() {
+    deleteModal(e = null) {
       // We pass the ID of the button that we want to return focus to
       // when the modal has hidden
-      this.$refs["my-modal"].toggle("#toggle-btn");
+      this.itemDelete = e;
+      this.$refs["modal-delete"].toggle("#toggle-btn");
     },
 
     getData: async function () {
@@ -233,12 +329,13 @@ export default {
         .then((resp) => resp.data)
         .catch((err) => console.log(err));
     },
-    postData: async function () {
+    postData: async function (data) {
+      console.log(data);
       return await axios
-        .create("http://localhost:8000/api/operadora/create", {
-          operadora: operadora,
-          descricao: descricao,
-          status: status,
+        .post("http://localhost:8000/api/operadora/create", {
+          operadora: data.operadora,
+          descricao: data.descricao,
+          status: data.status.toString(),
         })
         .then((resp) => resp.data)
         .catch((err) => console.log(err));
@@ -256,31 +353,46 @@ export default {
     deleteData: async function (id) {
       return await axios
         .update(`http://localhost:8000/api/operadora/delete/${id}`)
-        .then((resp) => resp.data)
+        .then((resp) => {
+          this.$refs["modal-delete"].toggle("#toggle-btn");
+          return resp.data;
+        })
         .catch((err) => console.log(err));
     },
   },
   created() {
     this.getData();
+    this.items.map((x) => {
+      this.operadoraDropDown.push({
+        value: x.operadora,
+        text: x.operadora,
+      });
+    });
   },
   computed: {
-    selectAll: {
-      get: function () {
-        return true
-          ? /*this.selected.length == this.users.length*/ true
-          : false;
-      },
-      set: function (value) {
-        var selected = [];
+    items() {
+      let arr = [
+        { status: 0, operadora: 40, descricao: "Dickerson" },
+        { status: 1, operadora: 21, descricao: "Larsen" },
+        { status: 1, operadora: 89, descricao: "Geneva" },
+        { status: 1, operadora: 38, descricao: "Jami" },
+      ];
 
-        // if (value) {
-        //   this.users.forEach(function (user) {
-        //     selected.push(user.id);
-        //   });
-        // }
+      if (this.selected != null && this.selectedOperadora != null) {
+        return arr.filter(
+          (x) =>
+            x.status == this.selected && this.selectedOperadora == x.operadora
+        );
+      }
 
-        this.selected = selected;
-      },
+      if (this.selected != null || this.selectedOperadora != null) {
+        return arr.filter(
+          (x) =>
+            x.status == this.selected || this.selectedOperadora == x.operadora
+        );
+      }
+
+      return arr;
     },
   },
   watch: {
@@ -288,6 +400,12 @@ export default {
       if (this.allRecords === "accepted")
         this.$refs.selectableTable.selectAllRows();
       else this.$refs.selectableTable.clearSelected();
+    },
+    selected() {
+      return this.items;
+    },
+    selectedOperadora() {
+      return this.items;
     },
   },
 };
--- serve/app/Models/Operadora.php
+++ serve/app/Models/Operadora.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Operadora extends Model
 {
-    protected $fillable = ['operadora'];
+    protected $fillable = ['operadora', 'descricao', 'status'];
 }
